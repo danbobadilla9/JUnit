4f01b6cf3cd5b4949ddb06962b4896cc13294eab
Israel Bobadilla Segundo <danbobadilla9@hotmail.com>
2024-09-24 10:43:54 -0600

    - Pruebas con assert 	-Uso de custom exceptions

--- JUnit5Curse/src/main/java/com/cm/dibs/ejemplos/models/Cuenta.java
+++ JUnit5Curse/src/main/java/com/cm/dibs/ejemplos/models/Cuenta.java
@@ -1,5 +1,7 @@
 package com.cm.dibs.ejemplos.models;
 
+import com.cm.dibs.ejemplos.exceptions.DineroInsuficienteException;
+
 import java.math.BigDecimal;
 
 public class Cuenta {
@@ -27,4 +29,59 @@ public class Cuenta {
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
     }
+
+
+
+    public void debito(BigDecimal monto){
+//        this.saldo.subtract(monto);// bigdecimal es inmutable, lo que hace es retornar una nueva instancia con ese saldo
+
+        BigDecimal nuevoSaldo = this.saldo.subtract(monto);
+        if(nuevoSaldo.compareTo(BigDecimal.ZERO) < 0){
+            throw new DineroInsuficienteException("Dinero insuficiente");
+        }
+        this.saldo = nuevoSaldo;
+    }
+
+    public void credito(BigDecimal monto){
+        this.saldo = this.saldo.add(monto);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public boolean equals(Object object){
+        // compara incluso si es null
+        if(!(object instanceof  Cuenta)){
+            return false;
+        }
+        Cuenta c = (Cuenta) object;
+        if(this.persona == null || this.saldo == null){
+            return false;
+        }
+
+        return this.persona.equals(c.getPersona()) && this.saldo.equals(c.getSaldo());
+    }
+
+
+
+
+
 }
--- JUnit5Curse/src/test/java/com/cm/dibs/ejemplos/models/CuentaTest.java
+++ JUnit5Curse/src/test/java/com/cm/dibs/ejemplos/models/CuentaTest.java
@@ -1,5 +1,6 @@
 package com.cm.dibs.ejemplos.models;
 
+import com.cm.dibs.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Test;
 // este es un import statico para no tener que llamar a las clases a cada rato
 import java.math.BigDecimal;
@@ -29,4 +30,67 @@ class CuentaTest {
         assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
         assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
     }
+
+    @Test
+    void testReferenciaCuenta() {
+        Cuenta cuenta = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+        Cuenta cuenta2 = new Cuenta("John Doe", new BigDecimal("8900.9997"));
+
+        //assertNotEquals(cuenta,cuenta2);
+        assertEquals(cuenta,cuenta2);
+
+    }
+
+    @Test
+    void testDebitoCuenta(){
+        Cuenta cuenta = new Cuenta("Israel", new BigDecimal("1000.12345"));
+        cuenta.debito(new BigDecimal("100")); // se va a restar
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900,cuenta.getSaldo().intValue());
+        assertEquals("900.12345",cuenta.getSaldo().toPlainString());
+    }
+
+
+    @Test
+    void testCreditoCuenta(){
+        Cuenta cuenta = new Cuenta("Israel", new BigDecimal("1000.12345"));
+        cuenta.credito(new BigDecimal("100")); // se va a sumar
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(1100,cuenta.getSaldo().intValue());
+        assertEquals("1100.12345",cuenta.getSaldo().toPlainString());
+    }
+
+    @Test
+    void testDineroInsuficienteException(){
+        Cuenta cuenta = new Cuenta("Israel", new BigDecimal("1000.12345"));
+        // este metodo regresa el tipo de exception
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.debito(new BigDecimal("1000000"));
+        } );
+        String actual = exception.getMessage();
+        String esperado = "Dinero insuficiente";
+        assertEquals(esperado,actual);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
